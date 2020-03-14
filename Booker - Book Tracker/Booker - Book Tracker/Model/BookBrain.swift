0e3207beb4f106afbf483f796f150c07b8fdb2b5
Dominik Drag <dominikdrag98@gmail.com>
2020-03-14 13:44:03 +0100

    Add BookBrain

---
+++ Booker - Book Tracker/Booker - Book Tracker/Model/BookBrain.swift
@@ -0,0 +1,11 @@
+//
+//  BookBrain.swift
+//  Booker - Book Tracker
+//
+//  Created by Dominik Drąg on 14/03/2020.
+//  Copyright © 2020 Dominik Drąg. All rights reserved.
+//
+
+import Foundation
+
+
