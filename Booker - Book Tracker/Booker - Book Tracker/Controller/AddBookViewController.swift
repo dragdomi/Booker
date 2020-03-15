3c1301723bda5c768a594941664887664ec96de0
Dominik Drag <dominikdrag98@gmail.com>
2020-03-15 17:23:46 +0100

    Update AddBookViewController.swift

--- Booker - Book Tracker/Booker - Book Tracker/Controller/AddBookViewController.swift
+++ Booker - Book Tracker/Booker - Book Tracker/Controller/AddBookViewController.swift
@@ -13,6 +13,10 @@ class AddBookViewController: UIViewController {
     @IBOutlet weak var bookAuthorTextField: UITextField!
     @IBOutlet weak var totalPagesTextField: UITextField!
     @IBOutlet weak var pagesReadTextField: UITextField!
+    @IBOutlet weak var beginDatePicker: UIDatePicker!
+    @IBOutlet weak var addBookButton: UIButton!
+    let viewController = ViewController()
+    
     
     var bookTitle: String?
     var bookAuthor: String?
@@ -21,7 +25,7 @@ class AddBookViewController: UIViewController {
     var beginDate: Date?
     
     override func viewDidLoad() {
-        
+        addBookButton.isEnabled = false
     }
     
     @IBAction func textFieldChanged(_ sender: UITextField) {
@@ -29,24 +33,40 @@ class AddBookViewController: UIViewController {
             switch sender.tag {
             case 0:
                 bookTitle = senderText
-                print(bookTitle)
             case 1:
                 bookAuthor = senderText
-                print(bookAuthor)
             case 2:
                 if let senderInt = Int(senderText) {
                     bookTotalPages = senderInt
-                    print(bookTotalPages)
                 }
             case 3:
                 if let senderInt = Int(senderText) {
                     bookPagesRead = senderInt
-                    print(bookPagesRead)
                 }
             default:
                 print("Out of cases")
             }
         }
+        activateButton()
+    }
+    @IBAction func beginDateChanged(_ sender: UIDatePicker) {
+        beginDate = sender.date
+        activateButton()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
+    func activateButton() {
+        if (bookTitle != nil) && (bookAuthor != nil) && (bookTitle != nil) && (bookTotalPages != nil) && (bookPagesRead != nil) && (beginDate != nil) {
+            addBookButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func addBookButtonPressed(_ sender: UIButton) {
+        viewController.bookBrain.addBook(title: bookTitle!, author: bookAuthor!, totalPages: bookTotalPages!, pagesRead: bookPagesRead!, beginDate: beginDate!, finishDate: nil)
+        self.dismiss(animated: true, completion: nil)
+    }
 }
