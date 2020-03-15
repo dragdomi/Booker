6b5dfb5b7232016e17f95fcdbbc7f7982b0a763e
Dominik Drag <dominikdrag98@gmail.com>
2020-03-15 17:23:36 +0100

    Update BookBrain.swift

--- Booker - Book Tracker/Booker - Book Tracker/Model/BookBrain.swift
+++ Booker - Book Tracker/Booker - Book Tracker/Model/BookBrain.swift
@@ -13,6 +13,5 @@ struct BookBrain {
     
     mutating func addBook(title: String, author: String, totalPages: Int, pagesRead: Int, beginDate: Date, finishDate: Date?) {
         books.append(BookModel(title: title, author: author, totalPages: totalPages, pagesRead: pagesRead, beginDate: beginDate, finishDate: finishDate))
-        print(books)
     }
 }
