b5fc31341ed4528f440c8275dd1077bd4473cbcb
Dominik Drag <dominikdrag98@gmail.com>
2020-03-25 19:21:13 +0100

    Create BookDetailsViewController.swift

---
+++ Booker - Book Tracker/Booker - Book Tracker/Controller/BookDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BookDetailsViewController.swift
+//  Booker - Book Tracker
+//
+//  Created by Dominik Drąg on 18/03/2020.
+//  Copyright © 2020 Dominik Drąg. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailsViewController: UIViewController {
+    var book: BookModel!
+}
