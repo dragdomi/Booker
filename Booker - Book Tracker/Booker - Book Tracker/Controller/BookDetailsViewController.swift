4a4a5529d4500bb6bf1eb0328e3b9c3b72759e74
Dominik Drag <dominikdrag98@gmail.com>
2020-03-26 18:48:23 +0100

    Update BookDetailsViewController.swift

--- Booker - Book Tracker/Booker - Book Tracker/Controller/BookDetailsViewController.swift
+++ Booker - Book Tracker/Booker - Book Tracker/Controller/BookDetailsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BookDetailsViewController: UIViewController {
+class BookDetailsViewController: UIViewController, AddBookViewControllerDelegate {
     var book: BookModel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
@@ -32,7 +32,28 @@ class BookDetailsViewController: UIViewController {
         } else {
             finishDateLabel.text = ""
         }
-        
-        
     }
+    
+    @IBAction func editBookDataButtonPressed(_ sender: UIButton) {
+        if let addBookViewController = storyboard?.instantiateViewController(identifier: "AddBook") as? AddBookViewController {
+            addBookViewController.delegate = self
+            addBookViewController.bookTitle = book.title
+            addBookViewController.bookAuthor = book.author
+            addBookViewController.bookTotalPages = book.totalPages
+            addBookViewController.bookPagesRead = book.pagesRead
+            addBookViewController.beginDate = book.beginDate
+            addBookViewController.finishDate = book.finishDate
+            navigationController?.pushViewController(addBookViewController, animated: true)
+        }
+    }
+    
+    func handleBookData(title: String, author: String, totalPages: Int, pagesRead: Int, beginDate: Date, finishDate: Date?) {
+        book.title = title
+        book.author = author
+        book.totalPages = totalPages
+        book.pagesRead = pagesRead
+        book.beginDate = beginDate
+        book.finishDate = finishDate
+    }
+    
 }
