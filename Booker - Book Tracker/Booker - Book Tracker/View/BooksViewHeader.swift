a302d1b65296755b1db97e9ff8b0582730434ed8
Dominik Drąg <dominikdrag@Dominiks-MacBook-Pro.local>
2020-09-07 17:22:25 +0200

    Update header

---
+++ Booker - Book Tracker/Booker - Book Tracker/View/BooksViewHeader.swift
@@ -0,0 +1,17 @@
+//
+//  BooksViewHeader.swift
+//  Booker - Book Tracker
+//
+//  Created by Dominik Drąg on 05/09/2020.
+//  Copyright © 2020 Dominik Drąg. All rights reserved.
+//
+
+import UIKit
+
+class BooksViewHeader: UITableViewHeaderFooterView {
+	static let height: CGFloat = 70
+	
+	@IBOutlet weak var headerLabel: UILabel!
+	@IBOutlet weak var headerButton: UIButton!
+
+}
