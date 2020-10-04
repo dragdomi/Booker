bf2ccc5ede164b01c726ccfb29c7fac52312a185
dragdominique <dominikdrag98@gmail.com>
2020-10-04 11:53:13 +0200

    Create UIColor.swift

---
+++ Booker - Book Tracker/Booker - Book Tracker/Extensions/UIColor.swift
@@ -0,0 +1,31 @@
+//
+//  UIColor.swift
+//  Booker - Book Tracker
+//
+//  Created by Dominik Drąg on 04/10/2020.
+//  Copyright © 2020 Dominik Drąg. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	static var color1: UIColor {
+		UIColor(named: "Color1")!
+	}
+	
+	static var color2: UIColor {
+		UIColor(named: "Color2")!
+	}
+	
+	static var color3: UIColor {
+		UIColor(named: "Color3")!
+	}
+	
+	static var color4: UIColor {
+		UIColor(named: "Color4")!
+	}
+	
+	static var interactiveColor: UIColor {
+		.systemIndigo
+	}
+}
