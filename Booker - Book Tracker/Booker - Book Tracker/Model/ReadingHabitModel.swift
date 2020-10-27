14a4c56f279082acdb334f8fd8e8dcb581f7f190
dragdominique <dominikdrag98@gmail.com>
2020-10-27 16:12:45 +0100

    Create ReadingHabitModel.swift

---
+++ Booker - Book Tracker/Booker - Book Tracker/Model/ReadingHabitModel.swift
@@ -0,0 +1,39 @@
+//
+//  ReadingHabitModel.swift
+//  Booker - Book Tracker
+//
+//  Created by Dominik Drąg on 27/10/2020.
+//  Copyright © 2020 Dominik Drąg. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ReadingHabitModel: Object, Codable {
+	@objc dynamic var date: String
+	@objc dynamic var pages: Int
+	var books = List<BookModel>()
+	
+	override var description: String { return "ReadingHabitModel {\(date), \(pages), \(books)}" }
+	
+	required init() {
+		self.date = ""
+		self.pages = 0
+		self.books = List<BookModel>()
+	}
+	
+	init(date: String, pages: Int, books: List<BookModel>) {
+		self.date = date
+		self.pages = pages
+		self.books = books
+	}
+	
+	enum CodingKeys: String, CodingKey {
+		case date = "date"
+		case pages = "pages"
+		case books = "books"
+	}
+	
+	
+	
+}
