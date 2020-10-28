f796678e3cc4eeda8c3b5af397782cf569392fbb
dragdominique <dominikdrag98@gmail.com>
2020-10-28 11:16:09 +0100

    Update ReadingEntryModel.swift

--- Booker - Book Tracker/Booker - Book Tracker/Model/ReadingEntryModel.swift
+++ Booker - Book Tracker/Booker - Book Tracker/Model/ReadingEntryModel.swift
@@ -14,6 +14,34 @@ class ReadingEntryModel: Object, Codable {
 	@objc dynamic var pages: Int
 	var books = List<BookModel>()
 	
+	func getDatePart(part: String) -> String? {
+		var startIndex: String.Index?
+		var endIndex: String.Index?
+		
+		switch part {
+		case "d", "day":
+			startIndex = date.startIndex
+			endIndex = date.index(date.startIndex, offsetBy: 2)
+		case "m", "month":
+			startIndex = date.index(date.startIndex, offsetBy: 3)
+			endIndex = date.index(date.startIndex, offsetBy: 5)
+		case "y", "year":
+			startIndex = date.index(date.startIndex, offsetBy: 6)
+			endIndex = date.index(date.startIndex, offsetBy: 10)
+		default:
+			startIndex = nil
+			endIndex = nil
+		}
+		
+		if let startIndex = startIndex, let endIndex = endIndex {
+			let dateSubString = date[startIndex..<endIndex]
+			let datePart = String(dateSubString)
+			return datePart
+		} else {
+			return nil
+		}
+	}
+	
 	override var description: String { return "ReadingHabitModel {\(date), \(pages), \(books)}" }
 	
 	required init() {
